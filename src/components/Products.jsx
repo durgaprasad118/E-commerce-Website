8ea6dbead6610f27b538473c075878c0dc5054d8
Achana Naga Durga Prasad <108184152+durgaprasad118@users.noreply.github.com>
2023-07-25 00:18:45 +0530

    Update Products.jsx

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -22,7 +22,7 @@ const Products = () => {
   useEffect(() => {
     const getProducts = async () => {
       setLoading(true);
-      const response = await fetch("https://dummyjson.com/products");
+      const response = await fetch("https://fakestoreapi.com/products/");
       if (componentMounted) {
         setData(await response.clone().json());
         setFilter(await response.json());
