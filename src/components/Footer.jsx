9b413ebadea4a5d189a970eb1c73ef43215a94a7
Achana Naga Durga Prasad <108184152+durgaprasad118@users.noreply.github.com>
2024-06-11 10:48:19 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <div className="d-flex align-items-center justify-content-center pb-5">
           <div className="col-md-6">
             <p className="mb-3 mb-md-0">Made with ❤️ by {" "}
-              <a  href="https://ssahibsingh.github.io" className="text-decoration-underline text-dark fs-5" target="_blank" rel="noreferrer">Achana Naga Durga Prasad</a>
+              <a  href="https://dp-portfolio-tawny.vercel.app/" className="text-decoration-underline text-dark fs-5" target="_blank" rel="noreferrer">Achana Naga Durga Prasad</a>
             </p>
             <a className="text-dark fs-4" href="https://github.com/durgaprasad118" target="_blank" rel="noreferrer">
               <i className="fa fa-github"></i>
